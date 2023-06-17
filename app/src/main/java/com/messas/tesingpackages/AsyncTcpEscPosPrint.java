41f6dbdc96b381b7b97c4e7a5cbf8c15b4673a9c
Arifuljava <arifulislam.java.c@gmail.com>
2023-06-17 16:01:17 +0600

    main

---
+++ app/src/main/java/com/messas/tesingpackages/AsyncTcpEscPosPrint.java
@@ -0,0 +1,14 @@
+package com.messas.tesingpackages;
+
+
+import android.content.Context;
+
+public class AsyncTcpEscPosPrint extends AsyncEscPosPrint {
+    public AsyncTcpEscPosPrint(Context context) {
+        super(context);
+    }
+
+    public AsyncTcpEscPosPrint(Context context, OnPrintFinished onPrintFinished) {
+        super(context, onPrintFinished);
+    }
+}
---
+++ app/src/main/java/com/messas/tesingpackages/Util.java
@@ -0,0 +1,27 @@
+package com.messas.tesingpackages;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import net.glxn.qrgen.android.QRCode;
+
+public class Util {
+
+    private Util() {
+
+    }
+
+    public static Bitmap decodeBase64(String base64) {
+        byte[] decodedByte = Base64.decode(base64, 0);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+
+    public static Bitmap getQrCode(String qrCode) {
+        // please see the dependency to create QRCode
+        // it's not included in Printama
+        return QRCode.from(qrCode).bitmap();
+    }
+
+}
